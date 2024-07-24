9915ffbfdd36f71844d49aa785df8694f0093be0
Lipun mahapatra <118988494+Likunmahapatra031@users.noreply.github.com>
2024-07-24 22:51:57 +0530

    DAY_8

---
+++ Day_8/function_with_input.py
@@ -0,0 +1,21 @@
+
+#Case1
+"""def greet():
+    print("Hellow")
+    print("How do you do?")
+    print("Is'n it weather nice today?")
+greet()
+"""
+
+#Case2
+"""def my_greet_name(name):
+    print(f"Hellow {name}")
+    print(f"How do you do {name}")
+my_greet_name("Likun")
+"""
+
+#case3
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"Where are you from? {location}")
+greet_with(name="likun",location="Nayagarh")
