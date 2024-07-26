078cf5f5f78cc18c2b36cac54cfc24cc4b9f503e
Lipun mahapatra <118988494+Likunmahapatra031@users.noreply.github.com>
2024-07-26 22:31:10 +0530

    Day_9
    100_Days_of_Python_programming_Challenge...9/100

---
+++ Day_9/nesting_list.py
@@ -0,0 +1,19 @@
+#Nested_List
+travel_log={
+    "France":{"cities_visited":["paris","lille","Dijon"],"total_visites":12},
+    "Germany":{"cities_visited":["Berlin","Hamburg","Stuttgart"],"total_visited":3},
+}
+
+#Nasting Dictionary in a List
+travel_log=[
+    {
+    "country":"France",
+    "cities_visited":["paris","Lille","Dijon"],
+    "total_visited":12
+},
+{
+    "country":"Germany",
+    "cities_visited":["Berlin","Hamburg","Stuttgart"],
+    "total_visited":3
+},
+]
---
+++ Day_9/py_Dictionary.py
@@ -0,0 +1,18 @@
+progrmming_dictionary ={"Bug":"An error in a program that prevents the program from running as expected..",
+                        "function":"A pice of code that you can easily call over and over again..",
+                }
+
+#Adding new word in dictionary
+progrmming_dictionary["Loop"]="the action of doing somthing over and over again.."
+print(progrmming_dictionary)
+
+#creating an empty dictionary.
+empty_dictionary={}
+
+#wipe an existing dictionary
+programming_dictionary ={""}
+#print(programming_dictionary)
+
+#Loop through a dictionary
+for thing in programming_dictionary:
+    print(thing)
